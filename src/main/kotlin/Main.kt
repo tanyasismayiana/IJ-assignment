8199c04719004fac32a1f106f5b2b295cb0f6269
tanyamayiana <tanyamayiana@gmail.com>
2022-02-16 21:36:38 +0300

    assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name: String = "Tanyasis Mayiana "
+    println(name)
+    var age = 21
+    println(age)
+    var number:String = "0718777705"
+    println(number)
+    var weight:Byte = 25
+    println(weight)
+    var kenyancitizen:Boolean = true
+    println(kenyancitizen)
+
+}
